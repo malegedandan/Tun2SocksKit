a2e57cdf4835b561da106a7290cacf03ffb9449b
哒哒哒 <17779916+malegedandan@users.noreply.github.com>
2024-01-18 13:40:22 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -22,7 +22,7 @@ let package = Package(
     ),
     .binaryTarget(
         name: "HevSocks5Tunnel",
-        url: "https://github.com/whatbugsyou/hev-socks5-tunnel/releases/download/v2.5.3/HevSocks5Tunnel.xcframework.zip",
+        url: "https://github.com/malegedandan/Tun2SocksKit/releases/download/2.5.3/HevSocks5Tunnel.xcframework.zip",
         checksum: "0a7c582a82b2a8b621cfb9ccde7bb8d61e59c43f36ae52f7258ad2417b607d1e"
     )
   ]
